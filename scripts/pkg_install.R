c9476140648123e9e7f47900e09485e1a74e2e44
Sara J. Smith <ssmith6@mtroyal.ca>
2025-06-04 19:48:15 -0600

    Update pkg_install.R

--- scripts/pkg_install.R
+++ scripts/pkg_install.R
@@ -1,3 +1,1 @@
-install.packages("readr", repos='http://cran.us.r-project.org')
-install.packages("magrittr", repos='http://cran.us.r-project.org')
-install.packages("dplyr", repos='http://cran.us.r-project.org')
+install.packages("tidyverse", repos='http://cran.us.r-project.org')
