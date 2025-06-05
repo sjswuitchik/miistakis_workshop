50a65ae333a1176f34573f4c11d3adc7b6a519ce
sjswuitchik <ssmith6@mtroyal.ca>
2025-06-04 17:01:45 -0700

    changing slurm sub script

--- scripts/slurm_test.sh
+++ scripts/slurm_test.sh
@@ -2,9 +2,9 @@
 #SBATCH --account=def-sjsmith
 #SBATCH --job-name=wordcount
 #SBATCH --time=00:05:00
-#SBATCH --mem=1G
+#SBATCH --mem=1000
 #SBATCH --cpus-per-task=1
-#SBATCH --output=wordcount_output.txt
+#SBATCH --output=output.txt
 
 # you can load your modules here if you need them for your scripts e.g., R
 bash ../scripts/wordcount.sh
