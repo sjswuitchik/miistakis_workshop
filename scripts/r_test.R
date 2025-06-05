8362c0917455fe0f536e3034c25acfad7cff3c16
Sara J. Smith <ssmith6@mtroyal.ca>
2025-06-04 19:48:58 -0600

    Update r_test.R

--- scripts/r_test.R
+++ scripts/r_test.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-df <- read_delim("../data/minData.csv") %>%
+df <- read_delim("../data/minData.csv", delim = ',') %>%
   mutate(spp = "G. aculeatus")
 
 write_delim(df, "minData.clean.csv")
